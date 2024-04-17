5b875a872bf31e65c345abbde64bcbc31cb05c2e
pronoy656 <pronoypaul56@gmail.com>
2024-04-17 17:13:29 +0600

    error page design done

--- src/Component/ErrorPage/ErrorPage.jsx
+++ src/Component/ErrorPage/ErrorPage.jsx
@@ -1,7 +1,20 @@
+import { Link } from "react-router-dom";
+
 const ErrorPage = () => {
   return (
     <div>
-      <h1>This is error page</h1>
+      <div className="flex justify-center mt-9">
+        <img
+          className="h-[450px] w-[450px]"
+          src="https://i.ibb.co/xD5dS5k/error.jpg"
+          alt=""
+        />
+      </div>
+      <div className="flex justify-center mt-10">
+        <Link to={"/"}>
+          <button className="btn bg-red-700 text-white">Go Back To Home</button>
+        </Link>
+      </div>
     </div>
   );
 };
--- src/Component/UserPRofile/UserProfile.jsx
+++ src/Component/UserPRofile/UserProfile.jsx
@@ -1,25 +1,53 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../AuthProvider/Authprovider";
 
 const UserProfile = () => {
+  const { user } = useContext(AuthContext);
   return (
     <div>
-      <div className="hero min-h-screen bg-slate-400 mb-6">
-        <div className="hero-content flex-col lg:flex-row">
-          <img
-            src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
-            className="max-w-sm rounded-lg shadow-2xl"
-          />
-          <div className="ml-12">
-            <h1 className="text-5xl font-bold ">Box Office News!</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
-            <Link to={"/updateProfile"}>
-              {" "}
-              <button className="btn btn-primary">Update Profile</button>
-            </Link>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content ">
+          <div className="text-center lg:text-left">
+            <img
+              className="rounded-full w-[530px] h-[400px]"
+              src={user.photoURL}
+              alt=""
+            />
+          </div>
+          <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <form className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">User Name</span>
+                </label>
+                <input
+                  type={user.displayName}
+                  placeholder={user.displayName}
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">User Photo Url</span>
+                </label>
+                <input
+                  type="photo"
+                  placeholder={user.photoURL}
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control mt-6">
+                <Link to={"/updateProfile"}>
+                  {" "}
+                  <button className="btn bg-black text-white w-full">
+                    Update Profile
+                  </button>
+                </Link>
+              </div>
+            </form>
           </div>
         </div>
       </div>
