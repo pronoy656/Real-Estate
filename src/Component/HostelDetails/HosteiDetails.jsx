5acde0c802561980acfb816d55e1e4f163f31db1
pronoy656 <pronoypaul56@gmail.com>
2024-04-17 21:56:55 +0600

    style add on hostel details page

--- src/Component/HostelDetails/HosteiDetails.jsx
+++ src/Component/HostelDetails/HosteiDetails.jsx
@@ -1,4 +1,4 @@
-import { useLoaderData, useParams } from "react-router-dom";
+import { Link, useLoaderData, useParams } from "react-router-dom";
 
 const HosteiDetails = () => {
   const allHostelDetails = useLoaderData();
@@ -10,14 +10,59 @@ const HosteiDetails = () => {
   console.log(singleDetails);
   // console.log(id, allHostelDetails);
   return (
-    <div>
-      <img src={singleDetails.image} alt="" />
-      <p>{singleDetails.title}</p>
-      <p>{singleDetails.description}</p>
-      <p>{singleDetails.price}</p>
-      <p>{singleDetails.status}</p>
-      <p>{singleDetails.area}</p>
-      <p>{singleDetails.location}</p>
+    <div className="md:flex justify-center gap-x-12 mt-14 mb-12">
+      <div className="w-[573px]">
+        <img className="rounded-2xl" src={singleDetails.image} alt="" />
+      </div>
+      <div className="w-[573px]">
+        <p className="text-4xl font-bold mb-5">{singleDetails.title}</p>
+        <p className="text-xl font-semibold text-red-400">
+          Description:{" "}
+          <span className="text-base font-medium text-black">
+            {singleDetails.description}
+          </span>
+        </p>
+        <p className="text-xl font-semibold text-red-400 mt-4 mb-3">
+          Facilities
+        </p>
+        {singleDetails.facilities.map((facility) => (
+          <li>
+            <span className="text-xl font-medium ">{facility}</span>
+          </li>
+        ))}
+        <div className="flex justify-between">
+          <p className="text-xl font-semibold text-red-400 mt-4">
+            Price:{" "}
+            <span className="text-xl font-medium text-black">
+              {singleDetails.price}
+            </span>
+          </p>
+          <p className="text-xl font-semibold text-red-400 mt-4">
+            Status:
+            <span className="text-xl font-medium bg-green-500 text-white p-2 rounded-2xl ml-2">
+              {singleDetails.status}
+            </span>
+          </p>
+        </div>
+        <div className="flex justify-between">
+          <p className="text-xl font-semibold text-red-400 mt-4">
+            Area:
+            <span className="text-xl font-medium text-black ml-2">
+              {singleDetails.area}
+            </span>
+          </p>
+          <p className="text-xl font-semibold text-red-400 mt-4">
+            Location:
+            <span className="text-xl font-medium text-black ml-2">
+              {singleDetails.location}
+            </span>
+          </p>
+        </div>
+        <Link to={"/"}>
+          {" "}
+          <button className="btn btn-outline mt-4 w-full">Back</button>
+        </Link>
+      </div>
     </div>
   );
 };
